6e4fd58e3f4bd76e531270c3e11d2dc3ae5bf53a
RomanSamborskyi <r.samborskyi@gmail.com>
2023-09-20 16:11:29 +0200

    Initial Commit

---
+++ Sweet&Fruit Bonanza/Sweet_Fruit_BonanzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sweet_Fruit_BonanzaApp.swift
+//  Sweet&Fruit Bonanza
+//
+//  Created by Roman Samborskyi on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Sweet_Fruit_BonanzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
